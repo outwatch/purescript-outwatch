ffffc10f554794147052b43618f4ce0f6cabdda8
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-03-12 12:52:05 +0100

    Simplify cartesian product

--- src/Helpers/Helpers.purs
+++ src/Helpers/Helpers.purs
@@ -1,12 +1,12 @@
 module Helpers where
 
-import Control.Bind (bind)
+import Control.Apply ((<*>))
 import Control.Monad.Eff (Eff)
 import Data.Foldable (foldl, traverse_)
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Prelude (Unit, id, map)
+import Prelude (Unit, id, map, (#))
 import RxJS.Observable (Observable, combineLatest, just)
 
 maybeToArr :: forall a. Maybe a -> Array a
@@ -18,8 +18,7 @@ forEachMaybe :: forall e. Maybe (Eff e Unit) -> Eff e Unit
 forEachMaybe m = traverse_ id (maybeToArr m)
 
 tupleMaybes :: forall a b. Maybe a -> Maybe b -> Maybe (Tuple a b)
-tupleMaybes ma mb =
-  bind ma (\a -> map (\b -> Tuple a b) mb)
+tupleMaybes ma mb = (ma # map Tuple) <*> mb
 
 combineLatestAll :: forall a. List (Observable a) -> Observable (List a)
 combineLatestAll list = foldl (\acc cur -> combineLatest (\x y -> y : x) acc cur) (just Nil) list
