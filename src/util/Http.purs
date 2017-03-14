7d966fb724a95aa9cae43d960976f07d2432836f
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-03-14 11:57:21 +0100

    Improve HTTP api

--- src/util/Http.purs
+++ src/util/Http.purs
@@ -6,53 +6,75 @@ module OutWatch.Http ( createHttpHandler
   , delete
   , options
   , head
+  , requestWithBody
+  , HttpHandler
   ) where
 
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Data.HTTP.Method (Method(..))
+import Data.StrMap (empty)
 import Network.HTTP.Affjax (AJAX)
-import OutWatch.Sink (Handler, createHandlerImpl)
-import Prelude (show, ($), (#))
-import RxJS.Observable (Observable, Request, Response, ajax, ajaxWithBody, switchMap, share)
+import OutWatch.Sink (Handler, Observer, createHandlerImpl)
+import Prelude (show, ($))
+import RxJS.Observable (Observable, Request, Response, ajax, ajaxWithBody, switchMap)
 
 
 createHttpHandler :: forall a e. Array a -> Handler (ajax :: AJAX | e) a
 createHttpHandler = createHandlerImpl
 
 
-get :: forall e. Handler (ajax :: AJAX | e) String -> Observable Response
+get :: forall eff a. (Observable a -> Observable String) -> HttpHandler (ajax :: AJAX | eff)  a
 get = requestWithUrl
 
-getWithBody :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+getWithBody :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 getWithBody = request GET
 
-post :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+post :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 post = request POST
 
-put :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+put :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 put = request PUT
 
-delete :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+delete :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 delete = request DELETE
 
-options :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+options :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 options = request OPTIONS
 
-head :: forall e. Handler (ajax :: AJAX | e) Request -> Observable Response
+head :: forall e a. (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
 head = request HEAD
 
+requestWithBody :: String -> String -> Request
+requestWithBody url body =
+  { url : url
+  , body : body
+  , timeout : 0
+  , headers : empty
+  , crossDomain : false
+  , responseType : ""
+  , method : show GET
+  }
+
+type HttpHandler eff input = { responses :: Observable Response, sink :: Observer eff input }
 
 
-request :: forall e. Method -> Handler (ajax :: AJAX | e) Request -> Observable Response
-request method handler =
-  switchMap handler.src
-    (\req -> unsafePerformEff $ ajaxWithBody (setResponseType req method))
-    # share
+requestWithUrl :: forall e a. (Observable a -> Observable String) -> HttpHandler (ajax :: AJAX | e) a
+requestWithUrl transform =
+  let handler = createHandlerImpl[]
+      transformed = transform handler.src
+      responses = switchMap transformed (\url -> unsafePerformEff $ ajax url)
+  in {responses, sink : handler.sink}
 
-requestWithUrl :: forall e. Handler (ajax :: AJAX | e) String -> Observable Response
-requestWithUrl handler =
-  switchMap handler.src (\url -> unsafePerformEff $ ajax url) # share
 
-setResponseType :: Request -> Method -> Request
-setResponseType req method =
-  req { responseType = show method }
+request :: forall e a. Method -> (Observable a -> Observable Request) -> HttpHandler (ajax :: AJAX | e) a
+request method transform =
+  let handler = createHandlerImpl[]
+      transformed = transform handler.src
+      responses = switchMap transformed
+        (\req -> unsafePerformEff $ ajaxWithBody (setMethod req method))
+  in {responses, sink : handler.sink}
+
+
+setMethod :: Request -> Method -> Request
+setMethod req method =
+  req { method = show method }
