2a6f65b6dc8b421a50e4f61aa24436c8022e8776
Rémi Vion <vion.remi@gmail.com>
2017-04-07 17:15:47 +0200

    misc example change

--- src/Ok.purs
+++ src/Ok.purs
@@ -2,29 +2,23 @@ module Ok where
 
 
 import Control.Alt (map)
-
+import Control.Alternative (pure)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Except.Trans (lift)
-
-
-import Data.Function (const)
-
-
-
-
-import OutWatch (build, button_, childShow_, click_, createHandler_, div_, h1_, h3_, img_, inputNumber_, input_, src_, text_, type_, ul_, valueShow_)
-
+import Data.Function (const, ($))
+import Data.Int (round)
+import Data.List.Lazy (replicateM)
+import Data.Show (show)
+import OutWatch (build, button_, childShow_, click_, createHandler_, div_, h1_, h3_, img_, inputNumber_, input_, max_, src_, text_, type_, ul_, valueShow_)
 import OutWatch.Core (render)
 import OutWatch.Dom.Types (VDom)
-import OutWatch.Monadic.Attributes (children_)
+import OutWatch.Monadic.Attributes (children_, style_)
 import OutWatch.Monadic.Core (mapSink, unsafeFirst)
 import OutWatch.Monadic.Store (createStore, Store)
 import OutWatch.Monadic.Types (HTML)
-
-
-import Prelude (Unit, bind, (#), (+), (-))
-import RxJS.Observable (unwrap)
+import Prelude (Unit, bind, (#), (*), (+), (-))
+import RxJS.Observable (interval, startWith, timer, unwrap)
 import Snabbdom (VDOM)
 
 data Action
@@ -48,30 +42,41 @@ view :: forall eff. Boolean -> HTML eff Unit
 view b = do
   store <- lift (createStore initialState update)
   div_ do
-    h1_ (text_ "counter-store example")
+    h1_ (text_ "counters store example")
     button_ do
         text_ "Decrement"
         click_ (store # mapSink (const Decrement))
     button_ do
-        text_ "Increment"
+        text_ "Incremsssent"
         click_ (store # mapSink (const Increment))
     h3_ do
         text_ "Counter: "
         childShow_ store.src
-    -- when b do 
-    --   view false
-      -- <- unwrap
-      -- children
+    
 
-    sliderEvents <- createHandler_ []
+    let initialSliderVal = 5.0
+
+    sliderEvents <- createHandler_ [initialSliderVal]
     let imageLists = sliderEvents.src
-          # map (\n -> img_ (src_ "testtest"))
+          # map \n -> replicateM (round n) do
+              t <- pure ((interval 1000
+                  # map (_ * 10)
+                  # startWith 0))
+              div_ do
+                style_ "display:inline-block;"
+                childShow_ t
+                img_ do 
+                  src_ "img.png"
+                  style_ "width:32px;heught:32px"
 
-    ul_ do 
+    ul_ do
       input_ do
         type_ "range"
         inputNumber_ sliderEvents
-        valueShow_ 0
+        valueShow_ initialSliderVal
+        max_ 10.0
+
+      childShow_ sliderEvents.src
       test <- lift (unwrap (map build imageLists))
       div_ (children_ test)
       
