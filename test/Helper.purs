5ac95df54c5bf406d70e47adbc4cabc26d81f56c
sloosch <simonlooschen@web.de>
2017-04-13 23:25:24 +0200

    Update dependencies and make outwatch compile with purs 0.11

---
+++ test/Helper.purs
@@ -0,0 +1,51 @@
+module Test.Helper (
+    unsafeFindElement
+  , createDomRoot
+  , afterAll
+  ) where
+
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
+import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Types (htmlDocumentToDocument)
+import DOM.HTML.Window (document)
+import DOM.Node.Document (createElement)
+import DOM.Node.Element (setAttribute)
+import DOM.Node.Node (appendChild, setTextContent)
+import DOM.Node.ParentNode (QuerySelector(QuerySelector), querySelector)
+import DOM.Node.Types (Document, Element, documentToParentNode, elementToNode)
+import Data.Maybe (Maybe(Just, Nothing))
+import Partial.Unsafe (unsafeCrashWith)
+import Prelude (Unit, bind, discard, pure, unit, ($), (<>))
+import Unsafe.Coerce (unsafeCoerce)
+
+
+unsafeFindElement :: forall e m. (MonadEff (dom :: DOM | e) m) => String -> m Element
+unsafeFindElement queryStr = liftEff do
+  doc <- getDocument
+  maybeElem <- querySelector (QuerySelector queryStr) (documentToParentNode doc)
+  case maybeElem of
+    Nothing -> unsafeCrashWith $ "Element for query '" <> queryStr <> "' was not found."
+    Just elem -> pure elem
+
+getDocument ::  forall e m. (MonadEff (dom :: DOM | e) m) => m Document
+getDocument = liftEff do
+  wndow <- window
+  doc <- document wndow
+  pure $ htmlDocumentToDocument doc
+
+createDomRoot :: forall e m. (MonadEff (dom :: DOM | e) m) => m Unit
+createDomRoot = liftEff do
+  doc <- getDocument
+  bdy <- unsafeFindElement "body"
+  node <- createElement "div" doc
+  child <- appendChild (elementToNode node) (elementToNode bdy)
+  setAttribute "id" "app" (unsafeCoerce child)
+  pure unit
+
+afterAll :: forall e m. (MonadEff (dom :: DOM | e) m) => m Unit
+afterAll = liftEff do
+  doc <- getDocument
+  node <- createElement "div" doc
+  bdy <- unsafeFindElement "body"
+  setTextContent "" (unsafeCoerce bdy)
