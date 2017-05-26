e5d70cb18e8547f8f6ee0e3b9b94e2456ab99736
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-05-26 10:00:14 +0200

    Internal cleanup

--- src/OutWatch/Helpers/Helpers.purs
+++ src/OutWatch/Helpers/Helpers.purs
@@ -2,23 +2,18 @@ module OutWatch.Helpers.Helpers where
 
 import Control.Apply ((<*>))
 import Control.Monad.Eff (Eff)
-import Data.Foldable (foldl, traverse_)
+import Data.Foldable (foldl, sequence_)
 import Data.List (List(..), (:))
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 import Data.Tuple (Tuple(..))
-import Prelude (Unit, id, map, (#))
+import Prelude (Unit, (<$>))
 import RxJS.Observable (Observable, combineLatest, just)
 
-maybeToArr :: forall a. Maybe a -> Array a
-maybeToArr m = case m of
-  (Just val) -> [val]
-  Nothing -> []
-
 forEachMaybe :: forall e. Maybe (Eff e Unit) -> Eff e Unit
-forEachMaybe m = traverse_ id (maybeToArr m)
+forEachMaybe m = sequence_ m
 
 tupleMaybes :: forall a b. Maybe a -> Maybe b -> Maybe (Tuple a b)
-tupleMaybes ma mb = (ma # map Tuple) <*> mb
+tupleMaybes ma mb = Tuple <$> ma <*> mb
 
 combineLatestAll :: forall a. List (Observable a) -> Observable (List a)
 combineLatestAll list = foldl (\acc cur -> combineLatest (\x y -> y : x) acc cur) (just Nil) list
